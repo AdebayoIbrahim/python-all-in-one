d47acaeed0dd0a54b09807f8e806d9313ef9915d
Ibrahim <abdulmumeenibroheem51@gmail.com>
2024-01-13 12:51:43 +0100

    rendering html in templates folder

--- web-development/links.py
+++ web-development/links.py
@@ -1,27 +1,27 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 #app.route decorator to create a link to our first webpage
 @app.route("/")
 def Home():
-    return "Welcome!"
+    return render_template("home.html")
 
 
 
 @app.route("/about")
 def about():
-    return "About page"
+    return render_template('about.html')
 
 @app.route("/contact")
 def Contact():
-    return "Contact Page"
+    return render_template('contact.html')
 
 
-#query parameters ,variable name in routes
-@app.route("/greet-<name>")
-def Greet(name):
-    return f"Welcome {name}, How are You Doing"
+# #query parameters ,variable name in routes
+# @app.route("/greet-<name>")
+# def Greet(name):
+#     return f"Welcome {name}, How are You Doing"
 
 #if its in the current diretory
 if __name__ == "__main__":
