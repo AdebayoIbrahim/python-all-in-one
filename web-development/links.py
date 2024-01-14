9b85aef5ab60302b158eaa9efe7f461c64cbc243
Ibrahim <abdulmumeenibroheem51@gmail.com>
2024-01-14 15:22:59 +0100

    removed comments..

--- web-development/links.py
+++ web-development/links.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask,render_template,request
-
+# from webdb import addDb
 t_f = os.path.join(os.path.dirname(__file__),'templates')
 
 app = Flask(__name__, template_folder=t_f)
@@ -20,20 +20,16 @@ def Login():
 def submit():
     if request.method == 'POST':
         result = request.form
+        # fn = result['fname']
+        # ln = result['lname']
+        # addDb(fn,ln)
         return render_template("details.html",details=result)
     else:
         return "Invalid Parameter"
-    # try:
-    #     return render_template("details.html")
-    # except Exception as error:
-    #     print('Error'.format(error))
-
     
-    
-
-# @app.route("/contact")
-# def Contact():
-#     return render_template("contact.html")
+#if its in the current diretory    
+if __name__ == "__main__":
+    app.run()    
 
 
 # #query parameters ,variable name in routes
@@ -41,6 +37,4 @@ def submit():
 # def Greet(name):
 #     return f"Welcome {name}, How are You Doing"
 
-#if its in the current diretory
-if __name__ == "__main__":
-    app.run()
+
